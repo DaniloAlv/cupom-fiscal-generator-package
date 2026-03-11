43b88513ecfb51a41475ce13c4ebc64ca94c8f15
Danilo <danilo_alves201090@hotmail.com>
2026-03-11 20:25:39 -0300

    feat: geração de cupom fiscal
    estruturação do projeto em biblioteca de classes e um projeto console para testes

---
+++ CupomFiscalGenerator/ServiceCollectionExtensions.cs
@@ -0,0 +1,15 @@
+﻿using CupomFiscalGenerator.Services;
+using CupomFiscalGenerator.Services.Abstraction;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CupomFiscalGenerator
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddCupomFiscalGenerator(this IServiceCollection services)
+        {
+            services.AddScoped<ICupomFiscalGenerator, GeneratorService>();
+            return services;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# cupom-fiscal-generator-package
-Biblioteca com o intuito de gerar um cupom fiscal com as informações prestador de serviços e/ou estabelecimento, listando os produtos adquiridos pelo cliente.
+# Pacote Cupom Fiscal Generator
+
+### Biblioteca com o intuito de testes para geração de cupom fiscal com as informações do prestador de serviços e/ou estabelecimento, 
+listando os produtos adquiridos pelo cliente.
